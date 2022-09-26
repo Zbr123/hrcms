1fe350fa86ebe4c7f54097b61909ee8533f22fd8
YahyaAmin <yahyaamin1997@gmail.com>
2022-09-26 17:52:59 +0500

    admin portal

---
+++ src/test/java/Pages/Android/Admin_Portal/DashboardPage.java
@@ -0,0 +1,23 @@
+package Pages.Android.Admin_Portal;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static Hooks.Base_Class.driver;
+
+public class DashboardPage {
+
+    public static String dashboard_heading_xpath = "//h1[normalize-space()='Hi Administrator!']";
+    public static String email_field_id = "email";
+    public static String password_field_id = "password";
+    public static String login_button_xpath = "//button[@title='Login']";
+
+    public DashboardPage() {}
+    public static WebElement get_dashboard_heading_xpath(){return driver.findElement(By.xpath(dashboard_heading_xpath));}
+    public static WebElement get_email_field_id(){return driver.findElement(By.id(email_field_id));}
+    public static WebElement get_password_field_id(){return driver.findElement(By.id(password_field_id));}
+
+    public static WebElement get_login_button_xpath(){return driver.findElement(By.xpath(login_button_xpath));}
+
+
+}
